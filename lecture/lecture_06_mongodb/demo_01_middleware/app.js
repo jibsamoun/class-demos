d12fab6ad616e397bf8aaa482038bc5618da0767
Kristen Thayer <kristenmthayer@gmail.com>
2025-01-27 09:22:50 -0800

    middleware

---
+++ lecture/lecture_06_mongodb/demo_01_middleware/app.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+
+import usersRouter from './routes/users.js';
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res, next){
+    console.log("this is middleware 1, first part", req, res);
+    next();
+    console.log("this is middleware 1, second part", req, res);
+})
+
+
+app.use('/users', usersRouter);
+
+export default app;
