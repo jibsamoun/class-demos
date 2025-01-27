54fa166be258400b14cecb7b45aec3190a088136
Kristen Thayer <kristenmthayer@gmail.com>
2025-01-27 14:43:03 -0800

    adding data

--- lecture/lecture_07_security_xss_sql/demo_02_sql_end/routes/users.js
+++ lecture/lecture_07_security_xss_sql/demo_02_sql_end/routes/users.js
@@ -1,5 +1,33 @@
 import express from 'express';
+import sqlite3 from 'sqlite3';
+
 var router = express.Router();
+
+// db
+let db = new sqlite3.Database(':memory:', (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('connected')
+})
+
+db.serialize(() => {
+  db
+  .run('CREATE TABLE people(first_name text, last_name text)')
+  .run(`INSERT INTO people(first_name, last_name)
+    VALUES  ("Kristen", "Thayer"),
+            ("Steven", "Universe"),
+            ("Greg", "Universe"),
+            ("Lisa", "Simpson"),
+            ("Agatha", "Harkness")
+  `)
+  .run('CREATE TABLE secret_table(message text)')
+  .run(`INSERT INTO secret_table (message)
+    VALUES  ('The password for Kristen is: pa55w0rd'),
+            ('The treasure is hidden on the 5th floor')
+  `)
+
+})
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
