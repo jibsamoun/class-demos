c4bcb8a557619f6bec4c189f3343386ff136ccd0
Kristen Thayer <kristenmthayer@gmail.com>
2025-02-05 10:23:17 -0800

    updates with final auth code

--- lecture/lecture_09_sessions_auth/demo-2-auth-end/routes/users.js
+++ lecture/lecture_09_sessions_auth/demo-2-auth-end/routes/users.js
@@ -3,26 +3,32 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  console.log(req.session);
-if(req.session.userid){
-  res.send('here is your user information: '+ req.session.userid)
+if(req.session.isAuthenticated) {
+  res.status(200).send("you are authenticated");
 } else {
-  res.send('error: you must be logged in')
+  res.status(500).send('you  must be logged in')
 }
+
+//   console.log(req.session);
+// if(req.session.userid){
+//   res.send('here is your user information: '+ req.session.userid)
+// } else {
+//   res.send('error: you must be logged in')
+// }
 
  // res.send('respond with a resource');
 });
 
-router.post('/login', function(req, res, next){
-  if (req.body.username == 'kmt' && req.body.password == 'pw') {
-    req.session.userid = "kristen thayer"
-    console.log('logged in!')
-    res.send('you logged in!')
-  } else {
-    console.log('NOT logged in')
-    res.send('wrong login, try again')
-  }
-})
+// router.post('/login', function(req, res, next){
+//   if (req.body.username == 'kmt' && req.body.password == 'pw') {
+//     req.session.userid = "kristen thayer"
+//     console.log('logged in!')
+//     res.send('you logged in!')
+//   } else {
+//     console.log('NOT logged in')
+//     res.send('wrong login, try again')
+//   }
+// })
 
 router.post('/logout', function(req, res, next){
   console.log('logging out');
