a446680b80b743b084880429b222dea22e961f47
jibsamoun <jibrilsamoun@gmail.com>
2025-02-18 13:57:29 -0800

    auth starter

--- lecture/lecture_09_sessions_auth/demo-2-auth-end/app.js
+++ lecture/lecture_09_sessions_auth/demo-2-auth-end/app.js
@@ -2,28 +2,7 @@ import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
-import sessions from 'express-session';
-
-import WebAppAuthProvider from 'msal-node-wrapper'
-
-const authConfig = {
-    auth: {
-   	clientId: "Client ID or Application ID HERE",
-    	authority: "https://login.microsoftonline.com/Paste_the_Tenant_directory_ID_Here",
-    	clientSecret: "Client or Application secret here (NOT THE 'secret id', but the 'secret value')",
-    	redirectUri: "/redirect"
-    },
-	system: {
-    	loggerOptions: {
-        	loggerCallback(loglevel, message, containsPii) {
-            	console.log(message);
-        	},
-        	piiLoggingEnabled: false,
-        	logLevel: 3,
-    	}
-	}
-};
-
+import sessions from 'express-session'; // needdat
 
 import usersRouter from './routes/users.js';
 
@@ -35,15 +14,13 @@ const __dirname = dirname(__filename);
 
 var app = express();
 
-app.enable('trust proxy');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-const oneDay = 1000 * 60 * 60 * 24;
+const oneDay = 1000 * 60 * 60 * 24; // When cookie expires
 
 app.use(sessions({
     secret: "my secret is super secret lkadsglkjah",
@@ -52,24 +29,10 @@ app.use(sessions({
     resave: false
 }))
 
-const authProvider = await WebAppAuthProvider.WebAppAuthProvider.initialize(authConfig);
-app.use(authProvider.authenticate());
-
-app.get('/signin', (req, res, next) => {
-    return req.authContext.login({
-        postLoginRedirectUri: "/", // redirect here after login
-    })(req, res, next);
-
-});
-app.get('/signout', (req, res, next) => {
-    return req.authContext.logout({
-        postLogoutRedirectUri: "/", // redirect here after logout
-    })(req, res, next);
-
-});
-app.use(authProvider.interactionErrorHandler());
-
-
 app.use('/users', usersRouter);
+
+app.listen(3001, 'localhost', () => {
+    console.log('App listening at http://localhost:3000')
+})
 
 export default app;
--- lecture/lecture_09_sessions_auth/demo-2-auth-end/routes/users.js
+++ lecture/lecture_09_sessions_auth/demo-2-auth-end/routes/users.js
@@ -3,32 +3,32 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-if(req.session.isAuthenticated) {
-  res.status(200).send("you are authenticated");
+  console.log(req.session);
+if(req.session.userid){
+  res.send('here is your user information: '+ req.session.userid)
 } else {
-  res.status(500).send('you  must be logged in')
+  res.send('error: you must be logged in')
 }
-
-//   console.log(req.session);
-// if(req.session.userid){
-//   res.send('here is your user information: '+ req.session.userid)
-// } else {
-//   res.send('error: you must be logged in')
-// }
 
  // res.send('respond with a resource');
 });
 
-// router.post('/login', function(req, res, next){
-//   if (req.body.username == 'kmt' && req.body.password == 'pw') {
-//     req.session.userid = "kristen thayer"
-//     console.log('logged in!')
-//     res.send('you logged in!')
-//   } else {
-//     console.log('NOT logged in')
-//     res.send('wrong login, try again')
-//   }
-// })
+/**
+ * When publishing on the internet ( i.e. A5 ) use a database to
+ * check usernames and passwords
+ * 
+ * When passwords are saved in the database they should be encrypted
+ */
+router.post('/login', function(req, res, next){
+  if (req.body.username == 'jbs' && req.body.password == 'pw') {
+    req.session.userid = "Jibril Samoun"
+    console.log('logged in!')
+    res.send('you logged in!')
+  } else {
+    console.log('NOT logged in')
+    res.send('wrong login, try again')
+  }
+})
 
 router.post('/logout', function(req, res, next){
   console.log('logging out');
